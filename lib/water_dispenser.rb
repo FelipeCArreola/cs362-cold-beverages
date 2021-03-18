d945e284de2bf8c12d419643708e733b93fbbcd0
Felipe Arreola <arreolaf@oregonstate.edu>
2021-03-18 12:23:39 -0700

    FINAL checkpoint- attempted to add Drain method for Class WaterDispenser, to test for method drain

--- lib/water_dispenser.rb
+++ lib/water_dispenser.rb
@@ -6,8 +6,19 @@ class WaterDispenser
     @reservoir = reservoir
   end
 
-  def dispense(vessel)
-    reservoir.drain(vessel.volume)
+  def dispense(vessel = Vessel.new)
+  drain(vessel.volume)
+  end
+
+  def drain(vessel_volume)
+    if @reservoir <= vessel_volume 
+      # TODO --- EROR- fix
+      @reservoir = @reservoir - vessel_volume 
+      puts "dispensed amount: #{vessel_volume}"
+      vessel_volume
+    else
+      puts "INVALID: cannot dispense more than what is inside the reservoir"
+    end
   end
 
 end
