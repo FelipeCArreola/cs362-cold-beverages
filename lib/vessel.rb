548e8105311381878278a7ab252ad69afc51b855
Felipe Arreola <arreolaf@oregonstate.edu>
2021-03-18 11:42:01 -0700

    added default parameters when initialized without values, added attr_reader for name & empty, added methods empty? and fill

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,19 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :volume, :name, :empty
 
-  def initialize(name, volume)
+  def initialize(name = '', volume = 0)
     @name = name
     @volume = volume
+    volume == 0 ? @empty = true : @empty = false
+  end
+
+  def empty?
+    @empty
+  end
+
+  def fill
+    @volume = 100
+    @empty = false
   end
 
 end
