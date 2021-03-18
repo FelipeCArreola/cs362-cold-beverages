a473a138874b9bbe76aabfbe3211c42df3fc018f
Felipe Arreola <arreolaf@oregonstate.edu>
2021-03-18 11:43:27 -0700

    adjusted tested parameters for matchers in tests

--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -4,19 +4,17 @@ describe 'A vessel for holding liquid' do
 
   it 'has a name and volume' do
     vessel = Vessel.new
-    expect(vessel.name).to eq('FAKE')
-    expect(vessel.volume).to eq(100)
+    expect(vessel.name).to eq('')
+    expect(vessel.volume).to eq(0)
   end
 
   it 'is initially empty' do
-    skip
-    vessel = Vessel.new('FAKE', 100)
+    vessel = Vessel.new
     expect(vessel).to be_empty
   end
 
   it 'is no longer empty when we fill it' do
-    skip
-    vessel = Vessel.new('FAKE', 100)
+    vessel = Vessel.new('FAKE')
     vessel.fill
     expect(vessel).to_not be_empty
   end
